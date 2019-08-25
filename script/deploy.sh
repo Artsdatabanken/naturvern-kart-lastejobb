e8d07313e4271a42c082e59c9ddb3146ff60d8ba
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-25 19:42:55 +0200

    Add deploy script

---
+++ script/deploy.sh
@@ -0,0 +1,2 @@
+scp build/naturvernområde_4326.geojson  grunnkart@hydra:~/tilesdata/Naturvernområde/polygon_med_undertyper.4326.geojson
+scp build/naturvernområde_25833.geojson  grunnkart@hydra:~/tilesdata/Naturvernområde/polygon_med_undertyper.32633.geojson
