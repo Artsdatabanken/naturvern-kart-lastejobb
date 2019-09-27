9f4210d784506f87ec6153c5afd46017fcf92f80
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-09-28 00:41:11 +0200

    Fjern redundant sortering

--- stages/transform/06_bbox.js
+++ stages/transform/06_bbox.js
@@ -5,9 +5,9 @@ let vo = io.lesDatafil("naturvernområde_4326.geojson");
 
 vo.features.forEach(v => {
   const node = meta[v.properties.id];
+  node.kode = node.id;
+  delete node.id;
   node.bbox = geospatial.axisAlignedBoundingBox(v.geometry.coordinates);
 });
 
-let r = json.objectToArray(meta);
-r = r.sort((a, b) => (a.id > b.id ? 1 : -1));
-io.skrivBuildfil("naturvernområde_meta.json", r);
+io.skrivBuildfil("naturvernområde_meta.json", meta);
