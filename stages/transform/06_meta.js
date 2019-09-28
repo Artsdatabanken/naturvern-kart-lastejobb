3a57ab3d623e877b1dbddc75c904c0b5b16f1a55
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-09-28 14:26:02 +0200

    Skriv kode som attributt

--- stages/transform/06_meta.js
+++ stages/transform/06_meta.js
@@ -3,11 +3,12 @@ const { geospatial, io, json } = require("lastejobb");
 const meta = io.lesDatafil("meta.json");
 let vo = io.lesDatafil("naturvernområde_4326.geojson");
 
+const r = [];
 vo.features.forEach(v => {
   const node = meta[v.properties.id];
-  node.kode = node.id;
-  delete node.id;
+  node.kode = "VV-" + parseInt(node.id.substring(2));
   node.bbox = geospatial.axisAlignedBoundingBox(v.geometry.coordinates);
+  r.push(node);
 });
 
-io.skrivBuildfil("naturvernområde_meta.json", meta);
+io.skrivBuildfil("naturvernområde_meta.json", r);
