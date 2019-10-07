1901fb353d1ea915bccd32f32e76118e0988def6
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-07 18:56:13 +0200

    Deploy script

--- stages/deploy/staging.sh
+++ stages/deploy/staging.sh
@@ -2,5 +2,9 @@ set -e
 
 echo Deploy polygon_med_undertyper.4326.geojson 
 scp build/polygon.4326.geojson grunnkart@hydra:~/tilesdata/Naturvernområde/polygon_med_undertyper.4326.geojson
+
 echo Deploy polygon_med_undertyper.25833.geojson 
 scp build/polygon.25833.geojson grunnkart@hydra:~/tilesdata/Naturvernområde/polygon_med_undertyper.25833.geojson
+
+echo Deploy kildedata.4326.geojson 
+scp build/kildedata.4326.geojson grunnkart@hydra:~/tilesdata/Naturvernområde/kildedata.4326.geojson
