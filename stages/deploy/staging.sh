cddc33aa11621b804b2eb63a131028ad8aa7854c
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-01-05 12:48:13 +0100

    Use similar file naming to geojson-subset-kart-lastejobb

--- stages/deploy/staging.sh
+++ stages/deploy/staging.sh
@@ -1,10 +1,10 @@
 set -e
 
 echo Deploy polygon_med_undertyper.4326.geojson 
-scp build/polygon.4326.geojson grunnkart@hydra:~/tilesdata/Naturvernområde/polygon_med_undertyper.4326.geojson
+scp build/polygon.4326.geojson grunnkart@hydra:~/tilesdata/Naturvernområde/polygon_no_dissolve.4326.geojson
 
 echo Deploy polygon_med_undertyper.25833.geojson 
-scp build/polygon.25833.geojson grunnkart@hydra:~/tilesdata/Naturvernområde/polygon_med_undertyper.25833.geojson
+scp build/polygon.25833.geojson grunnkart@hydra:~/tilesdata/Naturvernområde/polygon_no_dissolve.25833.geojson
 
 echo Deploy kildedata.4326.geojsonl
 scp temp/kildedata.4326.geojsonl grunnkart@hydra:~/tilesdata/Naturvernområde/
