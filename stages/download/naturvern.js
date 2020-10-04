349787cd26df2269451368f80f8613add744a58f
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-10-04 11:04:20 +0200

    WFS batching configuration

--- stages/download/naturvern.js
+++ stages/download/naturvern.js
@@ -1,7 +1,8 @@
 const { io, wfs } = require("lastejobb");
 
+const options = { batchSize: 900, offset: 0 }
 const url =
-  "https://kart.miljodirektoratet.no/arcgis/rest/services/vern/MapServer/0/query?where=1=1&outfields=*&f=geojson";
+  "https://kart.miljodirektoratet.no/arcgis/rest/services/vern/MapServer/0/query?where=1=1&outfields=*&f=geojson&resultRecordCount=${resultRecordCount}&resultOffset=${resultOffset}";
 
 io.mkdir("temp");
-wfs.mirror(url, "temp/kildedata.4326.geojsonl");
+wfs.mirror(url, "temp/kildedata.4326.geojsonl", options);
