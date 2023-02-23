ad584117040febd300bb220161492204f7b42241
stigat <stig.thorseth@artsdatabanken.no>
2023-02-23 14:18:28 +0100

    dockerops

--- stages/transform/04_reproject.js
+++ stages/transform/04_reproject.js
@@ -1,23 +1,20 @@
-const { processes } = require("@artsdatabanken/lastejobb");
-var dops = require("./docker_operations");
-//const {}  = require("07_simpler");
+const { processes, dockerops} = require("@artsdatabanken/lastejobb");
+//const {}  = require("@artsdatabanken/lastejobb");
+//var dops = require("./docker_operations");
+
+//const {}  = require("07_simpler");,
 try{
   //return reproject("naturvernområde_4326.geojson", "naturvernområde_25833.geojson");
-  var n = dops.start_ogr_containerNImage(dops.create_container_name('nkl-image'));
+  var n = dockerops.start_ogr_containerNImage(dockerops.create_container_name('nkl-image'));
   //simplify naturvernområde_25833
-  return reproj_w_docker(n, "naturvernområde_4326.geojson", "naturvernområde_25833.geojson");
-  dops.clean_container(n);
+  return dockerops.reproj_w_docker(n, "naturvernområde_4326.geojson", "naturvernområde_25833.geojson");
+  clean_container(n);
 }catch(e){
   console.error(e);
   console.error(e.stack);
 }
-function reproject(src, target, epsg = "EPSG:25833") {
-  processes.exec(
-    `ogr2ogr -f GeoJSON -t_srs ${epsg} temp/${target} temp/${src}`
-  );
-}
 
 function reproj_w_docker(dc_cont_name, src, target, epsg = "EPSG:25833"){
   var cmd = `ogr2ogr -f GeoJSON -t_srs ${epsg} tmp/${target} tmp/${src}`
-  dops.exec_docker(dc_cont_name, cmd);
+  dockerops.exec_docker(dc_cont_name, cmd);
 }
