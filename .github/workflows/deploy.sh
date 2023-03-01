ffe6c77e343b8d2c6dce2d6794a465828b88c766
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-01 14:46:28 +0100

    paths and arguments

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Make folder and copy files"
+ls -l
 mkdir naturvern
 cp ../../build/polygon.4326.geojson naturvern/polygon_med_undertyper.4326.geojson
 cp ../../build/polygon.25833.geojson naturvern/polygon_med_undertyper.25833.geojson
@@ -9,6 +10,6 @@ echo "Create archive"
 tar --directory=naturvern -zcf naturvern.tar.gz .
 
 echo "Send archive to host"
-sshpass -p ${{ scp_pass }} scp -v -o StrictHostKeyChecking=no naturvern.tar.gz  ${{ $scp_user }}@${{ $scp_dest }}/
+#sshpass -p $1 scp -v -o StrictHostKeyChecking=no naturvern.tar.gz  $2@$3/
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy naturvern-kart-lastejobb"}' ${{ secrets.POST_SLACK }}
+curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy naturvern-kart-lastejobb"}' $4
