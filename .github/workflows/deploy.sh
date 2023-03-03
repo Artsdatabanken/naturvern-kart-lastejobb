b5e315f9a4a6a2ae88faaf4e78205691ae624718
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-03 12:18:31 +0100

    rename artifact and cleaning up deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 echo "Make folder and copy files"
-ls -l
 mkdir naturvern
 cp build/polygon.4326.geojson naturvern/polygon_med_undertyper.4326.geojson
 cp build/polygon.25833.geojson naturvern/polygon_med_undertyper.25833.geojson
 cp temp/kildedata.4326.geojsonl naturvern/
-
-echo "Create archive"
-tar --directory=naturvern -zcf naturvern.tar.gz .
-
-echo "Send archive to host"
-#sshpass -p $1 scp -v -o StrictHostKeyChecking=no naturvern.tar.gz  $2@$3/
 
 curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy naturvern-kart-lastejobb"}' $1
