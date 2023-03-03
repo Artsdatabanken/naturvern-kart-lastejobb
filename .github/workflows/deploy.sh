b2b9396769d078d7fb6c101610b1a21589d9ee2f
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-03 12:01:25 +0100

    grumble

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,9 +2,9 @@
 echo "Make folder and copy files"
 ls -l
 mkdir naturvern
-cp ../../build/polygon.4326.geojson naturvern/polygon_med_undertyper.4326.geojson
-cp ../../build/polygon.25833.geojson naturvern/polygon_med_undertyper.25833.geojson
-cp ../../temp/kildedata.4326.geojsonl naturvern/
+cp build/polygon.4326.geojson naturvern/polygon_med_undertyper.4326.geojson
+cp build/polygon.25833.geojson naturvern/polygon_med_undertyper.25833.geojson
+cp temp/kildedata.4326.geojsonl naturvern/
 
 echo "Create archive"
 tar --directory=naturvern -zcf naturvern.tar.gz .
