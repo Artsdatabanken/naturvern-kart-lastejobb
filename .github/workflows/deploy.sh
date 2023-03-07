d148f81b313263c0ad12236a97d59b43d1aa858e
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-07 14:34:43 +0100

    keep it simple stupid

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,3 +4,6 @@ mkdir naturvern
 cp build/polygon.4326.geojson naturvern/polygon_med_undertyper.4326.geojson
 cp build/polygon.25833.geojson naturvern/polygon_med_undertyper.25833.geojson
 cp temp/kildedata.4326.geojsonl naturvern/
+cp build/polygon.4326.geojson destinationRepo/polygon_med_undertyper.4326.geojson
+cp build/polygon.25833.geojson destinationRepo/polygon_med_undertyper.25833.geojson
+cp temp/kildedata.4326.geojsonl destinationRepo/
