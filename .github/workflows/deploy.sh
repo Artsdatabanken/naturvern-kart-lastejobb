a0436bc8826f799a6b86eb2fbe40591e35ee08c2
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-10 13:43:54 +0100

    Nye typer til repo

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,4 +6,6 @@ cp build/polygon.25833.geojson naturvern/polygon_med_undertyper.25833.geojson
 cp temp/kildedata.4326.geojsonl naturvern/
 cp build/polygon.4326.geojson destinationRepo/polygon_med_undertyper.4326.geojson
 cp build/polygon.25833.geojson destinationRepo/polygon_med_undertyper.25833.geojson
+cp build/type.json destinationRepo/type.json
+cp build/type.schema.json destinationRepo/type.schema.json
 cp temp/kildedata.4326.geojsonl destinationRepo/
